aedad423b1607f0dfb676cb27e7fe6431b4d4c67
Nilam Cahya <nilyacahya4@gmail.com>
2022-06-09 12:01:24 +0700

    add database

--- config/database.js
+++ config/database.js
@@ -10,25 +10,43 @@ module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl:{
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl:{
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl:{
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
